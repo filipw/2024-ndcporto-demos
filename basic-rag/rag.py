17de2fabdcb84a83af2c680ef25d5fa64246fd12
filipw <filip@strathweb.com>
2024-10-06 14:43:24 +0200

    include vectors

--- basic-rag/rag.py
+++ basic-rag/rag.py
@@ -7,6 +7,9 @@ from openai import AzureOpenAI
 AZURE_OPENAI_RESOURCE = os.environ["AZURE_OPENAI_RESOURCE"]
 AZURE_OPENAI_KEY = os.environ["AZURE_OPENAI_KEY"]
 
+EMBEDDINGS_AZURE_OPENAI_RESOURCE = os.environ["EMBEDDINGS_AZURE_OPENAI_RESOURCE"]
+EMBEDDINGS_AZURE_OPENAI_KEY = os.environ["EMBEDDINGS_AZURE_OPENAI_KEY"]
+
 AZURE_AI_SEARCH_ENDPOINT = os.environ["AZURE_AI_SEARCH_ENDPOINT"]
 AZURE_AI_SEARCH_KEY = os.environ["AZURE_AI_SEARCH_KEY"]
 AZURE_AI_SEARCH_INDEX_NAME = os.environ["AZURE_AI_SEARCH_INDEX_NAME"]
@@ -16,8 +19,6 @@ azure_openai_client = AzureOpenAI(
             api_key=AZURE_OPENAI_KEY,
             azure_endpoint=f"https://{AZURE_OPENAI_RESOURCE}.openai.azure.com/",
         )
-
-print(f"https://{AZURE_OPENAI_RESOURCE}.openai.azure.com/")
 
 messages = [
     {
@@ -37,8 +38,23 @@ extra_body = {
             "type": "api_key"
         },
         "key": AZURE_AI_SEARCH_KEY,
-        "index_name": AZURE_AI_SEARCH_INDEX_NAME
-      }
+        "index_name": AZURE_AI_SEARCH_INDEX_NAME,
+        "top_n_documents": 3,
+        "query_type": "vector",
+        "embedding_dependency": {
+            "endpoint": f"https://{EMBEDDINGS_AZURE_OPENAI_RESOURCE}.openai.azure.com/openai/deployments/text-embedding-3-small/embeddings",
+            "type": "endpoint",
+            "authentication": {
+                "key": EMBEDDINGS_AZURE_OPENAI_KEY,
+                "type": "api_key"
+            }
+        },
+        "fields_mapping": {
+            "vector_fields": ["description_vector"],
+            "content_fields": ["name", "category", "price", "description"],
+            "title_field": "name"
+        }
+      },
     }
   ],
 }
@@ -56,7 +72,7 @@ try:
         stream = azure_openai_client.chat.completions.create(
             model="gpt-4o-mini",
             messages=messages,
-            max_tokens=100,
+            max_tokens=512,
             extra_body=extra_body,
             stream=True
         )
