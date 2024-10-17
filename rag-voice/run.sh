8ea439b2acc01931c3575eb32a8938ad8cfa1115
filipw <filip@strathweb.com>
2024-10-17 09:49:51 +0100

    updated the run.sh

--- rag-voice/run.sh
+++ rag-voice/run.sh
@@ -3,12 +3,5 @@
 cd Strathweb.Samples.Realtime.Rag
 
 ffplay -f s16le -ar 24000 user-question.pcm -autoexit -nodisp 
-
-mkfifo assistant-response.pcm
-
-ffplay -f s16le -ar 24000 assistant-response.pcm -autoexit -nodisp > /dev/null 2>&1 &
-FFPLAY_PID=$!
-dotnet bin/Release/net9.0/Strathweb.Samples.Realtime.Rag.dll
-wait $FFPLAY_PID
-
-rm assistant-response.pcm
+dotnet run -c Release
+ffplay -f s16le -ar 24000 assistant-response.pcm -autoexit -nodisp
